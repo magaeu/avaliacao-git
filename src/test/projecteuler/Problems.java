9b29665f8c5f8fac4806decfc6c84f29ea50eaa4
Gabriel <gabriel.azuirson@ufpe.br>
2013-03-15 11:39:26 -0300

    mudancas realizada para compilar codigo

--- src/test/projecteuler/Problems.java
+++ src/test/projecteuler/Problems.java
@@ -1,8 +1,11 @@
 package projecteuler;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import projecteuler.problems.Problem;
+import projecteuler.problems.Problem1;
+import projecteuler.problems.Problem2;
 
 /**
  * 
